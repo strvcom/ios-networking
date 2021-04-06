f079acc7ff4ea30e02d103483e5c48ed33d47e23
Tomas Cejka <tomas.cejka@strv.com>
2021-04-06 08:24:41 +0200

    [feat] add default authenticated flag for requestable

--- Sources/ios-networking/Core/Requestable.swift
+++ Sources/ios-networking/Core/Requestable.swift
@@ -38,6 +38,10 @@ public extension Requestable {
     var method: HTTPMethod {
         .get
     }
+    
+    var authenticated: Bool {
+        false
+    }
 
     var headers: [String: String]? {
         nil
