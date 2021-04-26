9a26e7335613cb0ff6b0bad8fbabcd0ad2df8258
Tomas Cejka <tomas.cejka@strv.com>
2021-04-26 09:39:37 +0200

    [feat] add test for logging interceptor

--- Tests/NetworkingTests/LoggingInterceptorTests.swift
+++ Tests/NetworkingTests/LoggingInterceptorTests.swift
@@ -1,14 +1,51 @@
 @testable import Networking
 import XCTest
+import Combine
 
 final class LoggingInterceptorTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+    enum MockRouter: Requestable {
+        case logging
+
+        var baseURL: URL {
+            // swiftlint:disable:next force_unwrapping
+            URL(string: "https://loggingInterceptor.tests")!
+        }
+
+        var path: String {
+            switch self {
+            case .logging:
+                return "logging"
+            }
+        }
+    }
+
+    func testNoEffect() {
+       // test logging interceptor doesn't effect request or response anyway
+
+        let mockEndpointRequest = EndpointRequest(MockRouter.logging)
+        let mockURLRequest = URLRequest(url: MockRouter.logging.baseURL)
+        // swiftlint:disable:next force_unwrapping
+        let mockURLResponse: URLResponse = HTTPURLResponse(url: MockRouter.logging.baseURL, statusCode: 200, httpVersion: nil, headerFields: nil)!
+        let mockResponse = (Data(), mockURLResponse)
+        let mockURLPublisher = Just(mockURLRequest).setFailureType(to: Error.self).eraseToAnyPublisher()
+        let mockResponsePublisher: AnyPublisher<Response, Error> = Just(mockResponse).setFailureType(to: Error.self).eraseToAnyPublisher()
+
+        let loggingInterceptor = LoggingInterceptor()
+        let adaptResult = awaitCompletion(for: loggingInterceptor.adapt(mockURLPublisher, for: mockEndpointRequest))
+
+        XCTAssertNoThrow(try adaptResult.get())
+        if let urlRequest = try? adaptResult.get().first {
+            XCTAssert(urlRequest == mockURLRequest)
+        }
+
+        let processResult = awaitCompletion(for: loggingInterceptor.process(mockResponsePublisher, with: mockURLRequest, for: mockEndpointRequest))
+        XCTAssertNoThrow(try processResult.get())
+        if let response = try? processResult.get().first {
+            XCTAssert(response.data == mockResponse.0 && response.response == mockResponse.1)
+        }
     }
 
     static var allTests = [
-        ("testExample", testExample)
+        ("testNoEffect", testNoEffect)
     ]
 }
