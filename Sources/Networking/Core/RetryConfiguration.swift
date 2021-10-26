edfe0b7b00234b7af8f820e6d986a0755e24213a
Tomas Cejka <tomas.cejka@strv.com>
2021-10-26 10:46:02 +0200

    [feat] docs

--- Sources/Networking/Core/RetryConfiguration.swift
+++ Sources/Networking/Core/RetryConfiguration.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 
+/// Retry of API calls allows various options wrapped into `RetryConfiguration` struct
 public struct RetryConfiguration {
+    /// Number of retries
     let retries: Int
+    /// Delay between each retry to avoid overwhelming API
     let delay: TimeInterval
+    /// Customization providing error to bool in case error should be retried, by default `404, 500` are not retried
     let retryHandler: (Error) -> Bool
 
     // default configuration ignores
