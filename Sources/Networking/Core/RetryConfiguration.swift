e26c5c9cb60d131ed793073ca74cf550633f8c32
Matej Molnár <matej.molnar@outlook.com>
2022-11-22 18:28:47 +0100

    feat: add DelayConfiguration

--- Sources/Networking/Core/RetryConfiguration.swift
+++ Sources/Networking/Core/RetryConfiguration.swift
@@ -12,7 +12,7 @@ public struct RetryConfiguration {
     /// The number of retries.
     let retries: Int
     /// The delay between each retry to avoid overwhelming API.
-    let delay: TimeInterval
+    let delay: DelayConfiguration
     /// A handler which determines wether a request should be retried or not based on an error.
     /// By default errors with status codes `404, 500` are not being retried.
     let retryHandler: (Error) -> Bool
@@ -20,7 +20,7 @@ public struct RetryConfiguration {
     // default configuration ignores
     static var `default` = RetryConfiguration(
         retries: 3,
-        delay: 2
+        delay: .constant(2)
     ) { error in
         guard let networkError = error as? NetworkError,
               case let .unacceptableStatusCode(statusCode, _, _) = networkError
@@ -29,5 +29,15 @@ public struct RetryConfiguration {
         }
 
         return statusCode != 404 && statusCode != 500
+    }
+}
+
+extension RetryConfiguration {
+    /// A type that defines the delay strategy for retry logic.
+    enum DelayConfiguration {
+        /// The delay cumulatively increases after each retry
+        case progressive(TimeInterval)
+        /// The delay is the same after each retry
+        case constant(TimeInterval)
     }
 }
