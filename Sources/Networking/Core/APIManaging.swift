566794e5ea389f1080bbebdae0e4ae0219694951
Matej Molnár <matej.molnar@outlook.com>
2022-11-22 18:19:14 +0100

    feat: add default JSON decoder

--- Sources/Networking/Core/APIManaging.swift
+++ Sources/Networking/Core/APIManaging.swift
@@ -12,6 +12,9 @@ import Foundation
 
 /// A definition of an API layer with methods for handling API requests.
 public protocol APIManaging {
+    /// A default JSONDecoder used for all requests
+    var defaultDecoder: JSONDecoder { get }
+    
     /// Creates a network request for an API endpoint defined by ``Requestable``.
     /// - Parameters:
     ///   - endpoint: API endpoint requestable definition.
@@ -36,6 +39,11 @@ public protocol APIManaging {
 // MARK: - Provide request with default json decoder, retry configuration
 
 public extension APIManaging {
+    /// Default JSONDecoder implementation
+    var defaultDecoder: JSONDecoder {
+        JSONDecoder()
+    }
+    
     /// Simplifies request using a default ``RetryConfiguration``.
     /// - Parameter endpoint: API endpoint requestable definition.
     /// - Returns: ``Response``.
@@ -50,13 +58,13 @@ public extension APIManaging {
     ///   - retryConfiguration: configuration for retrying behavior.
     /// - Returns: an object decoded from the response data.
     func request<DecodableResponse: Decodable>(_ endpoint: Requestable, retryConfiguration: RetryConfiguration?) async throws -> DecodableResponse {
-        try await request(endpoint, decoder: JSONDecoder(), retryConfiguration: retryConfiguration)
+        try await request(endpoint, decoder: defaultDecoder, retryConfiguration: retryConfiguration)
     }
 
     /// Simplifies request using a default `JSONDecoder` and default ``RetryConfiguration``.
     /// - Parameter endpoint: API endpoint requestable definition.
     /// - Returns: an object decoded from the response data.
     func request<DecodableResponse: Decodable>(_ endpoint: Requestable) async throws -> DecodableResponse {
-        try await request(endpoint, decoder: JSONDecoder(), retryConfiguration: RetryConfiguration.default)
+        try await request(endpoint, decoder: defaultDecoder, retryConfiguration: RetryConfiguration.default)
     }
 }
