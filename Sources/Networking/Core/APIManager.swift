39a5edf7ab2331e3a77ead482671d33da3c1509c
Matej Molnár <matej.molnar@outlook.com>
2022-11-28 16:06:03 +0100

    feat: add request implementation with retry logic

--- Sources/Networking/Core/APIManager.swift
+++ Sources/Networking/Core/APIManager.swift
@@ -12,6 +12,7 @@ open class APIManager {
     private let requestAdapters: [RequestAdapting]
     private let responseProcessors: [ResponseProcessing]
     private let urlSession: URLSession
+    private var retryCountDict = [String: Int]()
     
     // set identifier to URLSession if nil
     public init(
@@ -28,24 +29,71 @@ open class APIManager {
 extension APIManager: APIManaging {
     public func request(_ endpoint: Requestable, retryConfiguration: RetryConfiguration?) async throws -> Response {
         
+        print(Date())
         print(endpoint)
         print(retryConfiguration)
         
-        
         do {
-            
             /// create request
-
+            let endpointRequest = EndpointRequest(endpoint, sessionId: "")
+            var request = try endpoint.asRequest()
+            
             /// adapt request
+            request = try requestAdapters.adapt(request, for: endpointRequest)
             
             /// call request
+            var response = try await urlSession.data(for: endpoint.asRequest())
             
-            /// retry
+            guard let httpResponse = response.1 as? HTTPURLResponse else {
+                throw NetworkError.noStatusCode(response: response)
+            }
             
+            if
+                let acceptableStatusCodes = endpoint.acceptableStatusCodes,
+                !acceptableStatusCodes.contains(httpResponse.statusCode)
+            {
+                throw NetworkError.unacceptableStatusCode(
+                    statusCode: httpResponse.statusCode,
+                    acceptedStatusCodes: acceptableStatusCodes,
+                    response: response)
+            }
             
-           
+            /// process request
+            response = try responseProcessors.process(response, with: request, for: endpointRequest)
             
-            return try await urlSession.data(for: endpoint.asRequest())
+            /// reset retry count
+            retryCountDict[endpoint.identifier] = 0
+            
+            return response
+        } catch {
+            var retryCount = retryCountDict[endpoint.identifier] ?? 0
+            
+            guard
+                let retryConfiguration = retryConfiguration,
+                retryConfiguration.retryHandler(error),
+                retryConfiguration.retries > retryCount
+            else {
+                /// reset retry count
+                retryCountDict[endpoint.identifier] = 0
+                throw error
+            }
+            
+            /// retry request after delay
+            retryCount += 1
+            retryCountDict[endpoint.identifier] = retryCount
+            
+            let sleepDuration: UInt64 = {
+                switch retryConfiguration.delay {
+                case .constant(let timeInterval):
+                    return UInt64(timeInterval) * 1000000000
+                case .progressive(let timeInterval):
+                    return UInt64(timeInterval) * UInt64(retryCount) * 1000000000
+                }
+            }()
+            
+            try await Task.sleep(nanoseconds: sleepDuration)
+            
+            return try await request(endpoint, retryConfiguration: retryConfiguration)
         }
     }
 }
