e721ebd836c93ca86349bff0b08e075f11c64211
Matej Molnár <matej.molnar@outlook.com>
2023-01-04 11:17:55 +0100

    chore: remove sample button

--- NetworkingSampleApp/NetworkingSampleApp/Scene/SampleViewScene/SampleViewController.swift
+++ NetworkingSampleApp/NetworkingSampleApp/Scene/SampleViewScene/SampleViewController.swift
@@ -12,23 +12,7 @@ final class SampleViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let button = UIButton()
-        button.setTitle("Load users", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
-        button.addTarget(self, action: #selector(loadUsers), for: .touchUpInside)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        button.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(button)
-        
-        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
         viewModel.runNetworkingExamples()
-    }
-    
-    @objc private func loadUsers() {
-        Task {
-            try await viewModel.loadUserList()
-        }
     }
 }
