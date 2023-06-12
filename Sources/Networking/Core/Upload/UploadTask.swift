885c14de6799fa35ebbffc1133d0fc6537711176
Hoang Anh Ngo <tonyngo.jr@gmail.com>
2023-06-12 15:08:30 +0800

    feat: provide identity to task

--- Sources/Networking/Core/Upload/UploadTask.swift
+++ Sources/Networking/Core/Upload/UploadTask.swift
@@ -10,6 +10,8 @@ import Foundation
 
 /// Represents and manages an upload task and provides its state.
 public struct UploadTask {
+    public typealias ID = String
+
     /// The session task this object represents.
     let task: URLSessionUploadTask
 
@@ -18,4 +20,13 @@ public struct UploadTask {
 
     /// Use this publisher to emit a new state of the task.
     let statePublisher: CurrentValueSubject<State, Never>
+}
+
+extension UploadTask: Identifiable {
+    /// An unique task identifier.
+    ///
+    /// The identifier value is equal to the internal request's identifier that this task is associated with.
+    public var id: ID {
+        endpointRequest.id
+    }
 }
