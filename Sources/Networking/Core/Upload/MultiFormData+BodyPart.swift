65dc0967dcbc1e70eb863e2eff5fd1845a474a45
Hoang Anh Ngo <tonyngo.jr@gmail.com>
2023-06-18 14:20:56 +0800

    chore: define multiform data objects

---
+++ Sources/Networking/Core/Upload/MultiFormData+BodyPart.swift
@@ -0,0 +1,37 @@
+//
+//  MultiFormData+BodyPart.swift
+//  
+//
+//  Created by Tony Ngo on 18.06.2023.
+//
+
+import Foundation
+
+public extension MultiFormData {
+    struct BodyPart {
+        let dataStream: InputStream
+        let name: String
+        let fileName: String?
+        let mimeType: String?
+    }
+}
+
+extension MultiFormData.BodyPart {
+    var contentHeaders: [HTTPHeader.HeaderField: String] {
+        var disposition = "form-data; name=\"\(name)\""
+
+        if let fileName {
+            disposition += "; filename=\"\(fileName)\""
+        }
+
+        var headers: [HTTPHeader.HeaderField: String] = [
+            .contentDisposition: disposition
+        ]
+
+        if let mimeType {
+            headers[.contentType] = mimeType
+        }
+
+        return headers
+    }
+}
---
+++ Sources/Networking/Core/Upload/MultiFormData.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tony Ngo on 18.06.2023.
+//
+
+import Foundation
+
+open class MultiFormData {
+    
+}
