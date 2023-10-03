cde0508200cd4f0d5462a7fdad92c933e3da25fe
Tomas Cejka <ceja@centrum.cz>
2023-10-03 11:14:28 +0200

    [chore] update swiftlint, tools version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["Networking"]
         )
     ],
-    dependencies: [.package(url: "https://github.com/realm/SwiftLint.git", exact: "0.52.4")],
+    dependencies: [.package(url: "https://github.com/realm/SwiftLint.git", exact: "0.53.0")],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
