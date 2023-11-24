fbaa27b3c1537cd05a8a2c62f1c4d0945fcf1a25
Matej Molnar <matej.molnar@outlook.com>
2023-11-24 17:18:11 +0100

    [feat] conform RetryConfiguration to sendable

--- Sources/Networking/Core/RetryConfiguration.swift
+++ Sources/Networking/Core/RetryConfiguration.swift
@@ -8,19 +8,19 @@
 import Foundation
 
 /// Retry of API calls allows various options wrapped into `RetryConfiguration` struct.
-public struct RetryConfiguration {
+public struct RetryConfiguration: Sendable {
     /// The number of retries.
     let retries: Int
     /// The delay between each retry to avoid overwhelming API.
     let delay: DelayConfiguration
     /// A handler which determines wether a request should be retried or not based on an error.
     /// By default errors with status codes `HTTPStatusCode.nonRetriableCodes` are not being retried.
-    let retryHandler: (Error) -> Bool
+    let retryHandler: @Sendable (Error) -> Bool
 
     public init(
         retries: Int,
         delay: DelayConfiguration,
-        retryHandler: @escaping (Error) -> Bool) {
+        retryHandler: @Sendable @escaping (Error) -> Bool) {
         self.retries = retries
         self.delay = delay
         self.retryHandler = retryHandler
@@ -51,7 +51,7 @@ public struct RetryConfiguration {
 
 public extension RetryConfiguration {
     /// A type that defines the delay strategy for retry logic.
-    enum DelayConfiguration {
+    enum DelayConfiguration : Sendable {
         /// The delay cumulatively increases after each retry.
         case progressive(TimeInterval)
         /// The delay is the same after each retry.
