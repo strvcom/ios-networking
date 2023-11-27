e617286395b8e67ee4a10b44e14b076644bc809c
Matej Molnar <matej.molnar@outlook.com>
2023-11-27 18:07:39 +0100

    [feat] add FileManager sendable conformance

---
+++ Sources/Networking/Utils/FileManager+Sendable.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by Matej Molnár on 27.11.2023.
+//
+
+import Foundation
+
+// FileManager does not yet conforming to Sendable, hence we at least suppress the non-sendable warning.
+extension FileManager: @unchecked Sendable {}
