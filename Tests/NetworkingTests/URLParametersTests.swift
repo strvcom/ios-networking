24b04ed6f4108460977cf5dc0f088866a199d763
Tomas Cejka <ceja@centrum.cz>
2024-01-03 09:29:56 +0100

    [feat] update url percent encoded parameters tests

--- Tests/NetworkingTests/URLParametersTests.swift
+++ Tests/NetworkingTests/URLParametersTests.swift
@@ -35,27 +35,39 @@ final class URLParametersTests: XCTestCase {
         let router = Router.urlParameters(["date": dateString])
         let request = try router.asRequest()
 
+        guard let url = request.url else {
+            XCTFail("Can't create url from router")
+            return
+        }
+        
+        let queryItems = percentEncodedQueryItems(from: url)
         XCTAssertEqual(
-            request.url?.absoluteString,
-            "\(baseURLString)?date=\(dateString)"
+            queryItems.first(where: { $0.name == "date"})?.value,
+            dateString
         )
     }
 
     func testPlusSignPercentEncodedParameter() async throws {
         let dateString = "2023-11-29T12:13:04.598+0100"
-        let plusSignPercentEncodedString = "2023-11-29T12:13:04.598%2B0100"
+        let datePlusSignPercentEncodedString = "2023-11-29T12:13:04.598%2B0100"
         let router = Router.urlParameters(["date": PercentEncodedParameter(dateString, percentEncoding: .plusSign)])
         let request = try router.asRequest()
 
+        guard let url = request.url else {
+            XCTFail("Can't create url from router")
+            return
+        }
+        
+        let queryItems = percentEncodedQueryItems(from: url)
         XCTAssertEqual(
-            request.url?.absoluteString,
-            "\(baseURLString)?date=\(plusSignPercentEncodedString)"
+            queryItems.first(where: { $0.name == "date"})?.value,
+            datePlusSignPercentEncodedString
         )
     }
     
     func testMixedPlusSignPercentEncodedParameter() async throws {
         let dateString = "2023-11-29T12:13:04.598+0100"
-        let plusSignPercentEncodedString = "2023-11-29T12:13:04.598%2B0100"
+        let datePlusSignPercentEncodedString = "2023-11-29T12:13:04.598%2B0100"
         let searchString = "name+surname"
         
         let router = Router.urlParameters([
@@ -64,20 +76,74 @@ final class URLParametersTests: XCTestCase {
             ])
         let request = try router.asRequest()
         
+        guard let url = request.url else {
+            XCTFail("Can't create url from router")
+            return
+        }
+        
+        let queryItems = percentEncodedQueryItems(from: url)
         XCTAssertEqual(
-            request.url?.absoluteString,
-            "\(baseURLString)?date=\(plusSignPercentEncodedString)&search=\(searchString)"
+            queryItems.first(where: { $0.name == "date"})?.value,
+            datePlusSignPercentEncodedString
+        )
+        
+        XCTAssertEqual(
+            queryItems.first(where: { $0.name == "search"})?.value,
+            searchString
+        )
+    }
+    
+    func testMixedPercentEncodedParameter() async throws {
+        let dateString = "2023-11-29T12:13:04.598+0100"
+        let datePlusSignPercentEncodedString = "2023-11-29T12:13:04.598%2B0100"
+        let searchString = "name+surnam]e"
+        let searchPercentEncodedString = "name+surnam%5De"
+        
+        let router = Router.urlParameters([
+                "date": PercentEncodedParameter(dateString, percentEncoding: .plusSign),
+                "search": searchString
+            ])
+        let request = try router.asRequest()
+        
+        guard let url = request.url else {
+            XCTFail("Can't create url from router")
+            return
+        }
+        
+        let queryItems = percentEncodedQueryItems(from: url)
+        XCTAssertEqual(
+            queryItems.first(where: { $0.name == "date"})?.value,
+            datePlusSignPercentEncodedString
+        )
+        
+        XCTAssertEqual(
+            queryItems.first(where: { $0.name == "search"})?.value,
+            searchPercentEncodedString
         )
     }
     
     func testCustomPercentEncodedParameter() async throws {
         let customPercentEncodedString = "2023-11-29T12:13:04.598%2B+%0100"
-        let router = Router.urlParameters(["date": PercentEncodedParameter(customPercentEncodedString, percentEncoding: .plusSign)])
+        let router = Router.urlParameters(["date": PercentEncodedParameter(customPercentEncodedString, percentEncoding: .custom)])
         let request = try router.asRequest()
 
+        guard let url = request.url else {
+            XCTFail("Can't create url from router")
+            return
+        }
+        
+        let queryItems = percentEncodedQueryItems(from: url)
         XCTAssertEqual(
-            request.url?.absoluteString,
-            "\(baseURLString)?date=\(customPercentEncodedString)"
+            queryItems.first(where: { $0.name == "date"})?.value,
+            customPercentEncodedString
         )
+    }
+}
+
+private extension URLParametersTests {
+    // Helper method to create query items from URL to compare it with expected percent encoding
+    func percentEncodedQueryItems(from: URL) -> [URLQueryItem] {
+        let urlComponents = URLComponents(url: from, resolvingAgainstBaseURL: true)
+        return urlComponents?.percentEncodedQueryItems ?? []
     }
 }
