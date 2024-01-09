296c37222d49a2e6f6c8c313d45ca1418b15148a
Matej Molnar <matej.molnar@outlook.com>
2024-01-09 17:03:47 +0100

    [chore] remove unnecessary explicit Sendable conformance

--- NetworkingSampleApp/NetworkingSampleApp/Scenes/Users/User.swift
+++ NetworkingSampleApp/NetworkingSampleApp/Scenes/Users/User.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// Data structure of sample API user response
-struct User: Codable, Identifiable, Sendable {
+struct User: Codable, Identifiable {
     enum CodingKeys: String, CodingKey {
         case id
         case email
